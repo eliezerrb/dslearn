88226873f48c4e23f712193cfda4662c390c9a20
eliezerrb <eliezerrezende@outlook.com>
2022-09-13 21:40:15 -0300

    Renomeando pacotes

--- backend/src/main/java/com/eliezerrb/dslearnbds/DslearnbdsApplication.java
+++ backend/src/main/java/com/eliezerrb/dslearnbds/DslearnbdsApplication.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dslearnbds;
+package com.eliezerrb.dslearnbds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
