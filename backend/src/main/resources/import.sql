76386e56acdcd5badfd123b97409ed8591651171
eliezerrb <eliezerrezende@outlook.com>
2022-09-14 20:59:48 -0300

    Course

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://blog.coursify.me/wp-content/uploads/2019/10/create-videos-online-courses-cover-coursifyme.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf3m4Sr-tsahm067O64zrPJ3SkJht-mVyREQ&usqp=CAU');
