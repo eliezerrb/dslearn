52c7e07d083da85e3e3b82d5580f97d790cd1f7a
eliezerrb <eliezerrezende@outlook.com>
2022-10-06 18:07:04 -0300

    Conteúdo customizado para usuário logado

--- backend/src/main/java/com/eliezerrb/dslearnbds/repositories/NotificationRepository.java
+++ backend/src/main/java/com/eliezerrb/dslearnbds/repositories/NotificationRepository.java
@@ -1,9 +1,13 @@
 package com.eliezerrb.dslearnbds.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eliezerrb.dslearnbds.entities.Notification;
+import com.eliezerrb.dslearnbds.entities.User;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long>{
 
+	Page<Notification> findByUser(User user, Pageable pageable);
 }
---
+++ backend/src/main/java/com/eliezerrb/dslearnbds/resources/NotificationResource.java
@@ -0,0 +1,28 @@
+package com.eliezerrb.dslearnbds.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eliezerrb.dslearnbds.dto.NotificationDTO;
+import com.eliezerrb.dslearnbds.services.NotificationService;
+
+@RestController
+@RequestMapping(value = "/notifications")
+public class NotificationResource {
+	
+	@Autowired
+	private NotificationService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<NotificationDTO>> notificationForCurrentUser(Pageable pageable){
+		Page<NotificationDTO> page = service.notificationForCurrentUser(pageable);
+		return ResponseEntity.ok().body(page);
+	}
+	
+	
+}
